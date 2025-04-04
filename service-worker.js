85c4be5c48b265bbc2c42ea1f51bae860fe8250d
lpinta981 <108894335+lpinta981@users.noreply.github.com>
2025-04-04 15:46:46 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('v1').then(cache => {
+      return cache.addAll([
+        '/finaciamientos/',
+        '/finaciamientos/index.html',
+        '/finaciamientos/manifest.json',
+        'https://i.postimg.cc/PJzvpZrv/T-tulo-primario.png'
+      ]);
+    })
+  );
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
